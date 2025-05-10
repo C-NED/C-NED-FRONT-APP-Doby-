8ac5c0c581721c88519e6434dc2b3b36e333c266
lynnkrealm <akasarao3@gamil.com>
2025-05-10 23:31:52 +0900

    로그인 화면 구현 완료

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
+  <NavigationContainer initialRouteName="HomeScreen">
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
